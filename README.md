f3ac315680ef0b190a8065f487138af4657536ab
Rickert <rickert156@rambler.ru>
2024-08-03 13:07:21 +0400

    Add new Func(create project)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quniq Testing
+## Инструкция
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 main.py
+
+Далее выбираем кейсы, которые предлагает терминал
--- positive/createNewProject.py
+++ positive/createNewProject.py
@@ -7,18 +7,29 @@ def backButton(driver):
 	BACK_BUTTON = 'q-btn.q-btn-item.non-selectable.no-outline.q-btn--unelevated.q-btn--rectangle.bg-blue.text-white.q-btn--actionable.q-focusable.q-hoverable.q-btn--no-uppercase.q-btn--dense.base-button.base-button--large.q-px-md'
 	back_button = driver.find_element(By.CLASS_NAME, BACK_BUTTON)
 	back_button.click()
-	print('Нажал кнопку "Назад"')
+	print('Нажал кнопку "Назад", работает')
 	time.sleep(2)
 
 def nextButton(driver):
-	BUTTON_NEXT = 'q-btn.q-btn-item.non-selectable.no-outline.q-btn--unelevated.q-btn--rectangle.bg-blue.text-white.q-btn--actionable.q-focusable.q-hoverable.q-btn--no-uppercase.q-btn--dense.base-button.base-button--large.q-px-md'
+	BUTTON_NEXT = 'q-icon.notranslate.material-icons.icon-arrow-toright'
 	button_next = driver.find_element(By.CLASS_NAME, BUTTON_NEXT)
 	button_next.click()
 	time.sleep(2)
 	backButton(driver)
 	button_next.click()
+	print('Нажал кнопку "Продолжить", работает')
 	time.sleep(2)
 
+def role(driver):
+	name_point = 'q-radio__label.q-anchor--skip'
+	radio_button = 'q-radio__bg.absolute.non-selectable'
+	number_button=0
+	for button in driver.find_elements(By.CLASS_NAME,  radio_button):
+		number_button+=1
+		button.click()
+		time.sleep(0.5)
+		button = button.text
+	nextButton(driver)
 
 def createNewProject(driver):
 	number_button = 0
@@ -30,6 +41,7 @@ def createNewProject(driver):
 		listButton = listButton.text		
 		print(f'\t[{number_button}] {listButton}')
 	nextButton(driver)
+	role(driver)
 
 
 
